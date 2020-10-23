536478004682f5af9c22f9ddcfc7c3ca9d07f4e5
henriquesqs <henriquesqs@hotmail.com>
2020-10-23 16:12:46 -0300

    Updates

---
+++ precache-manifest.1aab9ba4b16ad0bc8e882f400060b30b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f0cb8c701b2653a2e68cddde8bc39e",
+    "url": "/Burger-Builder/index.html"
+  },
+  {
+    "revision": "6ed52c2cc1bd25027d8b",
+    "url": "/Burger-Builder/static/css/main.3889344b.chunk.css"
+  },
+  {
+    "revision": "2083ff8dcf6743f9f08d",
+    "url": "/Burger-Builder/static/js/2.1670a769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-Builder/static/js/2.1670a769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed52c2cc1bd25027d8b",
+    "url": "/Burger-Builder/static/js/main.a5f2bee9.chunk.js"
+  },
+  {
+    "revision": "a63305357e5bc69b1752",
+    "url": "/Burger-Builder/static/js/runtime-main.a76b48c8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
